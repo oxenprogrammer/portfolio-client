4026b34ef0e3317e68fa96b36ccca55c0d79f4e7
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-11 16:09:26 +0300

    Feat(root): add particle.js background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-client';
+
+  ngOnInit() {
+    particlesJS.load('particles-js', 'assets/data/particles.json', function () {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
