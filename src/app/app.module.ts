f7e1eead6566da1a50948f3147f3ad5c6f41011f
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-12 18:24:46 +0300

    Feat(profile): add style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,32 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ManagementModule } from './management/management.module';
 import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { UserModule } from './user/user.module';
 import { ProfileComponent } from './profile/profile.component';
+import { UserModule } from './user/user.module';
 import { WorkComponent } from './work/work.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, ProfileComponent, WorkComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ProfileComponent,
+    WorkComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
+    FlexLayoutModule,
     ManagementModule,
     UserModule,
   ],
