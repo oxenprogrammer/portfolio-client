d713329259432c974a3226c429bfdf452a19501a
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-13 04:16:49 +0300

    Feat(project): add previous works

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,15 @@
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
-const modules = [MatButtonModule, MatIconModule, MatToolbarModule];
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatCardModule,
+];
 @NgModule({
   declarations: [],
   imports: modules,
